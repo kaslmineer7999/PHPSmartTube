5e74e01d50b0910b340d135388b7aaf01025b990
kaslmineer7999 <kaslmineer7999github@outlook.com>
2026-04-25 22:06:05 +0200

    add 404 case instead of making using look at PHP warnings and errors

--- video.php
+++ video.php
@@ -13,6 +13,7 @@
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"/>
 <link rel="stylesheet" href="/SmartTube/VideoPlayerExtras.css"/>
 <?= $main ?>
+<?php if($row) { ?>
 <div class="video">
 	<div class="c-video">
 		<div class="video-wrapper"><video src="<?= htmlspecialchars($row['videoURL']) ?>">
@@ -42,11 +43,19 @@
 	padding: 1em; margin-left: 2em; margin-right: 2em; margin-top: 2em;
 	text-indent: -0.5em;
 " class="legacy-terminal"><?= $row['desc'] ?></p>
+<?php } else {
+	http_response_code(404); ?>
+<h1 style="margin-top: 0px">404 NOT FOUND!!</h1>
+<p style="font: italic 16px/12px serif; padding: 4px 16px;">You idiot, it's a 404 not found. It doesn't exist. Why looking for this</p>
+<?php
+	};
+?>
 <?= $footer1 ?>
 <?= $footer2 ?>
 <script src="/SmartTube/VideoPlayer.nomain.js"></script>
 <?= $footer3 ?>
 <?php
+	if(!$row) die('');
 	$stmt = $db->prepare('UPDATE videos SET views = views + 1 WHERE id = :id');
 
 	$stmt->bindValue(':id', $_GET['id'], SQLITE3_INTEGER);
