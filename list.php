05e0deafb8df6d7b61348848d89f83929ce80170
kaslmineer7999 <kaslmineer7999github@outlook.com>
2026-04-28 15:34:47 +0200

    add register

--- list.php
+++ list.php
@@ -11,7 +11,7 @@
 						<img src="<?= htmlentities($row['thumbURL']) ?>"/>
 					</a>
 				</div>
-				<div class="h-desc"><?= htmlentities($row['TITLE']) ?></div>
+				<div class="h-desc"><?= htmlentities($row['title']) ?></div>
 				<div class="filename"><?= htmlentities(mb_strimwidth($row['desc'], 0, 25, "...")); ?></div>
 				<?= $r ?>
 			</div>
--- register.php
+++ register.php
@@ -1,5 +1,6 @@
 <?php
 	require 'templator.php';
+	require 'env.php';
 ?>
 <?= $head ?>
 <style>
@@ -31,7 +32,7 @@
 	</div>
 	<div class="cf">
 		<label for="p">Secrecy Key:</label>
-		<input id="p" name="p" type="text"/>
+		<input id="p" name="p" type="text" disabled="disabled"/>
 	</div>
 	<button type="submit" class="h3d-button" style="font-size: 2em; position: absolute; bottom: 1em; left: 50%; transform: translateX(-50%)">
 		Register!
@@ -41,10 +42,27 @@
 	} else {
 		$db = new SQLite3('videos.db');
 		$stmt = $db->prepare('SELECT * FROM users WHERE username = :name');
-		$stmt->bindValue(':id', 1, SQLITE3_TEXT);
+		$stmt->bindValue(':name', $_POST['u'], SQLITE3_TEXT);
 		$result = $stmt->execute();
 		if($result->fetchArray(SQLITE3_ASSOC)) {
-			echo '<span style="color: red;"><marquee>User already exists!!</marquee></span>'
+			echo '<span style="color: red;"><marquee>User already exists!!</marquee></span>';
+		} else {
+			$my_time = time();
+			$stmt = $db->prepare('INSERT INTO users (timestamp, username, key) VALUES (:time, :name, :key)');
+			$stmt->bindValue(':time', $my_time, SQLITE3_INTEGER);
+			$stmt->bindValue(':name', $_POST['u'], SQLITE3_TEXT);
+
+			$key = hash_hmac('sha512', $_POST['u'] . $_POST['p'] . bin2hex(random_bytes(16)), $TOPSECRET);
+			$stmt->bindValue('key', password_hash($key, PASSWORD_BCRYPT, ["cost" => 12]), SQLITE3_TEXT);
+			$results = $stmt->execute();
+			if($results) {?>
+				Your key: <?= $key ?><br>
+				<textarea style="display:none;" aria-hidden="true" id="textarea"></textarea>
+				<button class="h3d-button" onclick="textarea.value = '<?= $key ?>'; textarea.focus(); textarea.select(); document.execCommand('copy');">Copy to clipboard</button>
+<?php
+			} else {
+				echo '<span style="color: red;">Can\'t make user account</span>;
+			}
 		}
 	}
 ?>
