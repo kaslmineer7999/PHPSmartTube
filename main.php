ee4cacb900d3ebba4e7003a444eb82d7722a87b8
kaslmineer7999 <kaslmineer7999github@outlook.com>
2026-04-30 13:49:50 +0200

    final

--- main.php
+++ main.php
@@ -1,6 +1,6 @@
 <?php
 	return call_user_func(function() {
-		if(isset($_COOKIE['username']) && isset($_COOKIE['session_id'])) {
+		if(!(empty($_COOKIE['username']) && empty($_COOKIE['session_id']))) {
 			$replacement = <<<EOV
 				<div class="cf" style="margin-bottom: 1em; font-size: 1.25em; text-align: center;">{$_COOKIE['username']}</div>
 				<div class="cf" s>
@@ -11,8 +11,8 @@
 			EOV;
 		} else {
 			$replacement = <<<EOV
-				<div style="padding-top: 0.75em"></div>
-				<div class="cf">
+				<div style="padding-top: 1.5em"></div>
+				<div class="cf" style="width: 450px;">
 					<div style="float: left; width: 50%; text-align: left;"><a href="/login.php" style="color:#00bfff;">Login</a></div>
 					<div style="float: left; width: 50%; text-align: right;"><a href="/register.php" style="color:#00bfff;">Register</a></div>
 				</div>
