b32c32fd4bf13d7042f39d09096740dbf8cfd0a5
kaslmineer7999 <kaslmineer7999github@outlook.com>
2026-05-01 13:09:19 +0200

    logout.php: Stupid me, man

--- logout.php
+++ logout.php
@@ -1,6 +1,6 @@
 <?php
-	header('Set-Cookie: username=; HttpOnly; Max-Age=86400; Secure; SameSite=Lax', false);
-	header('Set-Cookie: session_id=; HttpOnly; Max-Age=86400; Secure; SameSite=Lax', false);
+	header('Set-Cookie: username=; HttpOnly; Max-Age=0; Secure; SameSite=Lax', false);
+	header('Set-Cookie: session_id=; HttpOnly; Max-Age=0; Secure; SameSite=Lax', false);
 	header('Location: ' . urldecode($_GET['return']), false);
 ?>
 
