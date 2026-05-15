4cce8e8375134c9772b57497282b7c89cd2e27d1
kaslmineer7999 <kaslmineer7999github@outlook.com>
2026-05-15 15:31:05 +0200

    .

--- src/upload.php
+++ src/upload.php
@@ -3,5 +3,11 @@
 $app = require __DIR__ . '/init.php';
 
 ?>
-
 <?= $app['layout']['head'] ?>
+<?= $app['layout']['main'] ?>
+
+upload things
+
+<?= $app['layout']['footers'][0] ?>
+<?= $app['layout']['footers'][1] ?>
+<?= $app['layout']['footers'][2] ?>
